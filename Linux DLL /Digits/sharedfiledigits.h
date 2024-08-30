67de9581f2a4c627eb220bc385fe6eea7db09ff3
Maitreya Gangurde <94222925+Maitreya444@users.noreply.github.com>
2024-08-31 01:51:31 +0530

    Add files via upload

---
+++ Linux DLL /Digits/sharedfiledigits.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void ReverseDisplayDigit();
+void CheckZero();
+void CountTwo();
+void CountFour();
+void CountSix();
+void CountEven();
+void CountOdd();
+void CountBetween3and7();
+void DigitsMultiply();
+void Summation();
