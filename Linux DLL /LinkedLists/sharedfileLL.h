366bc243956edbe7d1b38a144f19bd66703bb851
Maitreya Gangurde <94222925+Maitreya444@users.noreply.github.com>
2024-09-22 22:04:16 +0530

    Update sharedfileLL.h

--- Linux DLL /LinkedLists/sharedfileLL.h
+++ Linux DLL /LinkedLists/sharedfileLL.h
@@ -16,3 +16,20 @@ void DisplaySLL(PNODE Head);
 int CountSLL(PNODE Head);
 void InsertAtPosSLL(PPNODE Head, int iNo, int iPos);
 void DeleteAtPosSLL(PPNODE Head, int iPos);
+
+typedef struct nodedll
+{
+    int data;
+    struct nodedll * next;
+    struct nodedll * prev;
+}NODEDLL, *PNODEDLL, **PPNODEDLL;
+
+void UserInputDLL();
+void InsertFirstDLL(PPNODEDLL Head, int iNo);
+void InsertLastDLL(PPNODEDLL Head, int iNo);
+void DeleteFirstDLL(PPNODEDLL Head);
+void DeleteLastDLL(PPNODEDLL Head);
+void DisplayDLL(PNODEDLL Head);
+int CountDLL(PNODEDLL Head);
+void InsertAtPosDLL(PPNODEDLL Head, int iNo, int iPos);
+void DeleteAtPosDLL(PPNODEDLL Head, int iPos);
